daf35251ba3a9ff1be07d86eb60b9929e5cd1b0d
Martin <martinaretap@gmail.com>
2024-09-05 20:19:33 +0200

    6

--- app/database/db.js
+++ app/database/db.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
-const URL = process.env.MONGO_URL
 
-console.log(URL)
+const URL = process.env.MONGO_URL;
 
+console.log("MongoDB URL:", URL);
 
-const connect = async () =>{
-    try{
-        await mongoose.connect(URL)
-        console.log("connected to db")
-
+const connect = async () => {
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to DB");
+    } catch (err) {
+        console.error("Error in connecting to DB:", err);
     }
-    catch(err){
-        console.log("error in connecting to db")
-    }
-}
+};
 
-export default connect
+export default connect;
