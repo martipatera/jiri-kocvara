8c96e45a8fc9d5989baeea7dc0f6e2ae018c846d
Martin <martinaretap@gmail.com>
2024-09-13 22:08:12 +0200

    a

--- app/(pages)/muj_trenink/page.jsx
+++ app/(pages)/muj_trenink/page.jsx
@@ -11,12 +11,11 @@ function MujTrenink() {
   const [messages, setMessages] = useState([])
 
 
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 
   const fetchMessages = async () => {
     try {
-      const res = await axios.get(apiUrl+"/api/get_messages");
+      const res = await axios.get("/api/get_messages");
       const data = await res.data
       
       setMessages(data.messages); // Uložení hodnot do stavu
