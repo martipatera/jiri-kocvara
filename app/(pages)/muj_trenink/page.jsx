bd8da69be91e94a976c7942b3de3bc71b4e9398c
Martin <martinaretap@gmail.com>
2024-09-13 12:18:17 +0200

    fix

--- app/(pages)/muj_trenink/page.jsx
+++ app/(pages)/muj_trenink/page.jsx
@@ -60,8 +60,8 @@ function MujTrenink() {
             <h1 className=''>Musíš se přihlásit nebo registrovat abys mohl pokračovat</h1>
 
             <span className='flex'>
-              <button className="bg-orange border-2 rounded-full p-1 xl:p-3 whitespace-nowrap text-white hover:brightness-110 hover:scale-105 transition-all"><Link href="/register">Registrovat se</Link></button>
-              <button className="bg-orange border-2 rounded-full p-1 xl:p-3 text-white hover:brightness-110 hover:scale-105 transition-all"><Link href="/login">Přihlásit se</Link></button>
+              <button className="bg-orange border-2 rounded-full p-1 xl:p-3 whitespace-nowrap text-black hover:brightness-110 hover:scale-105 transition-all"><Link href="/register">Registrovat se</Link></button>
+              <button className="bg-orange border-2 rounded-full p-1 xl:p-3 text-black hover:brightness-110 hover:scale-105 transition-all"><Link href="/login">Přihlásit se</Link></button>
             </span>
             
           </div>
