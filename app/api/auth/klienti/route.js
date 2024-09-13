a1e0e22144e4a9a1c5627083d7b5cadede518791
Martin <martinaretap@gmail.com>
2024-09-13 17:28:18 +0200

    response

--- app/api/auth/klienti/route.js
+++ app/api/auth/klienti/route.js
@@ -17,13 +17,21 @@ export const GET = async () =>{
             id: user._id
         }))
 
-        return new NextResponse(JSON.stringify({ 
+        const response = NextResponse(JSON.stringify({ 
             message: "Nalezeno",
             users
             
             
             
-         }), { status: 200 });
+         }), { status: 200 })
+
+        // Zakázání cache v hlavičkách
+        response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+        response.headers.set("Expires", "0");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Surrogate-Control", "no-store");;
+
+        
     }
     catch(err){
         return NextResponse.error(); // Vrátí chybu 500
--- app/api/get_messages/route.js
+++ app/api/get_messages/route.js
@@ -2,28 +2,29 @@ import { NextResponse } from "next/server";
 import connect from "@/app/database/db";
 import MessageSchema from "@/app/models/MessageSchema";
 
+export const GET = async () => {
+    try {
+        await connect();
 
-export const GET = async () =>{
-    try{
-
-        await connect()
-
-        const existingMessages = await MessageSchema.find();
-
-        const messages = existingMessages.map((message)=>({
+        const existingMessages = await MessageSchema.find({});
+        const messages = existingMessages.map((message) => ({
             author: message.author,
             subject: message.subject,
             message: message.message,
             created: message.createdAt,
             id: message._id
-        }))
+        }));
 
-        return new NextResponse(JSON.stringify({ 
-            messages,     
-            
-         }), { status: 200 });
-    }
-    catch(err){
+        const response = new NextResponse(JSON.stringify({ messages }), { status: 200 });
+
+        // Zakázání cache v hlavičkách
+        response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+        response.headers.set("Expires", "0");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Surrogate-Control", "no-store");
+
+        return response;
+    } catch (err) {
         return NextResponse.error(); // Vrátí chybu 500
     }
-}
+};
