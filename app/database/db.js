cb56cd6f8ef45294e4216bf8c8f7d8e5550608af
Martin <martinaretap@gmail.com>
2024-09-14 07:59:59 +0200

    a

--- app/database/db.js
+++ app/database/db.js
@@ -6,7 +6,7 @@ console.log("MongoDB URL:", URL);
 
 const connect = async () => {
     try {
-        await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, ssl:true });
+        await mongoose.connect(URL);
         console.log("Connected to DB");
     } catch (err) {
         console.error("Error in connecting to DB:", err);
