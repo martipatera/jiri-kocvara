6866c2b5649017117e1093f349c2f82ea107eb59
Martin <martinaretap@gmail.com>
2024-09-19 12:05:44 +0200

    fix

--- app/components/CarouselComponent.jsx
+++ app/components/CarouselComponent.jsx
@@ -8,12 +8,12 @@ import Counter from "./CountUp"
 function CarouselComponent() {
   return (
     
-    <div className="h-[275px]  md:h-44 lg:h-44 xl:h-80 2xl:h-44 my-0 text-white bg-gray-500 opacity-90 lg:px-0">
+    <div className="h-[265px] md:h-44 lg:h-44 xl:h-80 2xl:h-32 my-0 text-white bg-gray-500 opacity-100 lg:px-0">
       <Carousel slideInterval={4000}>
         
             {
                 carouselData.map((data, index)=>{
-                    return <div key={index} className="text-center px-10 md:px-14 2xl:h-24 lg:w-3/4">
+                    return <div key={index} className="text-center pb-7 md:pb-4  2xl:pb-5 px-10 md:px-14 xl:px-20 lg:w-3/4">
                         <p>{data.name}</p>
                         <p className="italic 2xl:px-0">&ldquo;{data.review}&rdquo;</p>
                     </div>
--- app/data/carouselData.js
+++ app/data/carouselData.js
@@ -2,17 +2,17 @@ const carouselData = [
 
     {
         name: "Jmeno P.1",
-        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl ddajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasj lkasjd lasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     }, 
 
     {
         name: "Jmeno P.2",
-        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl ddajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasj lkasjd lasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     }, 
 
     {
         name: "Jmeno P.3",
-        review: "blablalblkkldajladlsalkdjasll d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasj d lkasjd lasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     }, 
 
     {
@@ -32,22 +32,22 @@ const carouselData = [
 
     {
         name: "Jmeno P.7",
-        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ldajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     },
 
     {
         name: "Jmeno P.8",
-        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjasjd lasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     },
 
     {
         name: "Jmeno P.9",
-        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjasl d lkasjd lasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     },
 
     {
         name: "Jmeno P.10",
-        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d dajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjlkasjd lasj"
+        review: "blablalblkkldajladlsalkdjasl djlajdlasljdjasl d lkasjd ladajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjdajladlsalkdjasl djlajdlasljdjasl d lkasjd lasjsj"
     },
 
 
