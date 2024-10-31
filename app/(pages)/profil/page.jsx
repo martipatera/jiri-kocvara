a676eaf04549b097f2f6da7901c68ae16c1c2ecb
Martin <martinaretap@gmail.com>
2024-10-31 20:21:58 +0100

    texty

--- app/(pages)/profil/page.jsx
+++ app/(pages)/profil/page.jsx
@@ -38,13 +38,46 @@ function Profil() {
               
  
 
-              <div className='flex md:px-8 lg:hidden 2xl:flex 2xl:h-full flex-row px-2 justify-start items-center text-center lg:w-[500px] xl:w-[800px] lg:text-start text-lg md:text-lg  flex-wrap break-words pt-10 xl:pt-0 pb-5'>
-                <p>Lorem ipsum, um, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametum, dolor sit ametdolor sit amet consectetur adipisicing elit. Placeat totam doloribus soluta numquam eaque voluptatibus commodi quis, dolorum aliquam labore. Alias eos itaque, voluptatum quo nemo dolorem quam eius fugiat eum animi error expedita repellendus at corrupti quos ratione exercitationem. Neque quae expedita ex itaque sunt natus fuga dolor pariatur, ea aliquid alias repellat. Voluptas sapiente eveniet laudantium nihil fuga aspernatur quibusdam harum quam? Harum, praesentium quos ratione repellat deserunt consequatur. Officiis commodi dignissimos quibusdam, vitae ad magni labore ea sunt unde est ratione obcaecati voluptas culpa incidunt at consequatur iure nam modi perspiciatis dicta delectus atque? Ducimus ab nam nihil eligendi! Assumenda sunt asperiores aliquid laborum nostrum laudantium ab placeat voluptatum tempora ad, libero necessitatibus aspernatur omnis autem, temporibus exercitationem ea quidem! Quae nulla illo quaerat consequuntur totam ea labore a voluptas unde deleniti, magni impedit reiciendis deserunt dicta reprehenderit autem. Sequi nesciunt veniam, nemo, cum vero neque quae voluptatum ipsum aspernatur aliquam, cupiditate eligendi! Id sunt dolores aspernatur corrupti provident fuga quisquam autem omnis dolorem commodi reprehenderit, magni, reiciendis nostrum esse. Eveniet eaque assumenda aliquam autem totam qui impedit, magnam dolores minima amet alias. Voluptatum aliquid molestias dolores mollitia tempore. Pariatur quas dolorem nihil, esse facilis saepe tempora.1</p>
+              <div className=' flex md:px-8 lg:hidden 2xl:flex 2xl:h-full flex-row px-2 justify-start items-center text-center lg:w-[500px] xl:w-[800px] lg:text-start text-lg md:text-lg  flex-wrap break-words pt-10 xl:pt-0 pb-10'>
+                <p> <strong>Kdo jsem a jak pracuji?</strong><br></br>
+                    Trénování v posilovně, vedení lidí k dosažení progresu a mnohdy i zlepšení kvality běžného života se věnuji stabilně již  <strong>více než 10 let.</strong> Věřím, 
+                    že právě díky mojí filozofii, se kterou ke své práci přistupuji, mohu s lidmi pracovat full-time, aniž bych byl nucen mít k trénovaní ještě další práci. 
+                    To mi za posledních několik let umožňuje neustále se dál vzdělávat a následně <strong>- to absolutně nejdůležitější –</strong>  aplikovat tyto znalosti v praxi s mými klienty, 
+                    aby dosahovali svých cílů bez zbytečných mainstreamových bullshitů a jejich výsledky byly <strong>dlouhodobě udržitelné.</strong> <br></br><br></br>
+                    <strong> A jaká je moje filozofie?</strong><br></br>
+                    Za mě je jedním z hlavních cílů trenéra pracovat s klientem tak, aby se klient zároveň učil pracovat se sebou i on sám. 
+                    Proto se během naší spolupráce snažím klienta edukovat nejen <strong>CO</strong> má dělat, ale i <strong>PROČ.</strong> To vše děláme společně v reálném životě s reálnými výzvami a překážkami. 
+                    Spolupráce znamená <strong>PRACOVAT SPOLU</strong>, tedy určitě nečekej, že jsem “čaroděj” který ti sestaví zázračný plán a s minimálním úsilím dosáhneš výsledku
+                     “za pouhých 5 týdnů”. <strong>Nevyhnutelně budeš makat.</strong> A naší spoluprací získáš jistotu, že tvoje úsilí, vynaložená energie a změny, které uděláš, 
+                     budou mít smysl a dosáhneš dlouhodobě udržitelného progresu.
+                    Největší část mojí klientely tvoří stabilní základna <strong>začínajících, středně pokročilých nebo kondičních / hobby cvičenců. </strong> 
+                    Chystám také <strong>závodníky</strong> do různých kategorií kulturistiky <strong>(jak mužské, tak ženské kategorie). </strong> 
+                    Mám zkušenost s nastavením silového tréninku po různých  <strong>úrazech a operačních zákrocích.</strong> Za svou více než <strong>10letou</strong> trenérskou kariéru 
+                    jsem odtrénoval již <strong>nižší stovky klientů. </strong>
+                    S nemalým počtem klientů spolupracujeme pravidelně již mnoho let a můžeme tak dosahovat i pokročilejších výsledků. Spousta spokojených klientů se 
+                    ke mně vrací i opakovaně.
+                    <strong> Vzhledem k nesporným benefitům silového tréninku si nesmírně vážím každého, kdo se rozhodne, ať už s mojí pomocí či jinak, silový trénink zařadit 
+                    do svého života.</strong>
+                    </p>
               </div>
 
-              <div className='hidden pl-3 lg:flex 2xl:hidden flex-row px-2 justify-start items-center text-center xl:w-full xl:px-10 lg:text-start text-lg md:text-lg flex-wrap break-words'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cumque officiis nostrum esse eligendi earum ea. Unde et sequi odio suscipit iusto rerum, assumenda voluptatem! Saepe aperiam tempora ab ipsum omnis, unde velit quis eligendi laborum, itaque, delectus quae magnam. Ab ratione, alias aliquid deleniti cum natus dolorum aut a amet nulla sint error commodi harum repudiandae sed fugiat nisi ipsam voluptatum ducimus qui adipisci, minus voluptate! Earum sapiente odio quod. Blanditiis eos culpa voluptas quis qui aliquam ipsa beatae nemo quod iste quo, nostrum recusandae reiciendis vel accusantium nobis harum. Sapiente eius neque quam repudiandae debitis cumque, hic quasi explicabo molestias, eligendi quae iure temporibus amet, iusto odit vitae architecto voluptates autem nulla in id officiis incidunt beatae. Reprehenderit tempora dolores molestias laboriosam dignissimos fuga. Quas error consectetur voluptates assumenda temporibus commodi provident doloribus aliquam modi, quis ipsa in, exercitationem tempora tempore. Ab, doloremque molestias? Ut labore non ad nesciunt cupiditate quidem quo. Dolorum, aliquam harum est iure praesentium necessitatibus voluptas in exercitationem quod quidem laudantium adipisci officia neque incidunt iste saepe corporis, cumque sed illo delectus deserunt nisi enim excepturi. Tenetur blanditiis eveniet, vitae neque illo at voluptatum doloremque, maxime, porro commodi laboriosam explicabo beatae fugiat iusto quo veniam rerum nam vel tempora atque ea dicta quasi. Minima fugiat reiciendis veniam quam, maiores corporis voluptatum inventore eos nemo ullam incidunt dolor, sunt earum repellat esse provident cumque ut dicta! Eius voluptatibus asperiores quasi mollitia debitis fuga voluptas? Aliquid quod dicta aliquam quasi soluta dolorum possimus officia repellendus atque.2</p>
-                 </div>
+              <div className='hidden pl-3 pb-5 lg:flex 2xl:hidden flex-row px-8 justify-start items-center text-center xl:w-full xl:px-10 lg:text-start text-lg md:text-lg flex-wrap break-words'>
+                  <p><strong>Kdo jsem a jak pracuji?</strong><br></br>
+                    Trénování v posilovně, vedení lidí k dosažení progresu a mnohdy i zlepšení kvality běžného života se věnuji stabilně již <strong>více než 10 let.</strong> Věřím, 
+                    že právě díky mojí filozofii, se kterou ke své práci přistupuji, mohu s lidmi pracovat full-time, aniž bych byl nucen mít k trénovaní ještě další práci. 
+                    To mi za posledních několik let umožňuje neustále se dál vzdělávat a následně <strong>- to absolutně nejdůležitější –</strong> aplikovat tyto znalosti v praxi s mými klienty, 
+                    aby dosahovali svých cílů bez zbytečných mainstreamových bullshitů a jejich výsledky byly <strong>dlouhodobě udržitelné.</strong>
+                    <br></br><br></br><strong> A jaká je moje filozofie?</strong><br></br>
+                    Za mě je jedním z hlavních cílů trenéra pracovat s klientem tak, aby se klient zároveň učil pracovat se sebou i on sám. 
+                    Proto se během naší spolupráce snažím klienta edukovat nejen <strong>CO </strong> má dělat, ale i <strong>PROČ.</strong> To vše děláme společně v reálném životě s reálnými výzvami a překážkami. 
+                    Spolupráce znamená <strong>PRACOVAT SPOLU</strong>, tedy určitě nečekej, že jsem “čaroděj” který ti sestaví zázračný plán a s minimálním úsilím dosáhneš výsledku
+                     “za pouhých 5 týdnů”. <strong>Nevyhnutelně budeš makat.</strong> A naší spoluprací získáš jistotu, že tvoje úsilí, vynaložená energie a změny, které uděláš, 
+                     budou mít smysl a dosáhneš dlouhodobě udržitelného progresu.
+                    Největší část mojí klientely tvoří stabilní základna <strong>začínajících, středně pokročilých nebo kondičních / hobby cvičenců. </strong> 
+                    <strong>závodníky</strong> do různých kategorií kulturistiky <strong>(jak mužské, tak ženské kategorie).</strong> 
+                    Mám zkušenost s nastavením silového tréninku po různých <strong>úrazech a operačních zákrocích.</strong>   
+                    </p>
+              </div>
 
             
 
@@ -61,8 +94,13 @@ function Profil() {
                 >
               </Image>
 
-              <span className='hidden lg:block xl:hidden 2xl:hidden pt-4'>
-                <p className='lg:text-start text-lg md:text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam commodi nulla id, natus, officiis neque recusandae aperiam, consequatur nisi doloribus vitae molestias veritatis alias incidunt. Voluptate tempore provident officiis veritatis itaque saepe nisi debitis corporis atque quod omnis, porro ullam sequi nostrum quia amet delectus doloremque deleniti minus ipsa mollitia. Eveniet consectetur nostrum aspernatur porro blanditiis nisi ullam explicabo, ipsum dolore incidunt et nulla voluptate optio totam rem facilis veniam officiis quis? Quas corrupti est, illo perspiciatis reiciendis at consectetur vero dignissimos. Ad quos nesciunt dolorem, dolores adipisci explicabo sed rem rerum voluptatum obcaecati, eaque nam debitis suscipit, reiciendis distinctio.3</p>
+              <span className='hidden lg:block xl:hidden 2xl:hidden pt-20'>
+                <p className='lg:text-start text-lg md:text-lg'>Za svou více než <strong>10letou</strong> trenérskou kariéru 
+                    jsem odtrénoval již <strong>nižší stovky klientů. </strong> S nemalým počtem klientů spolupracujeme pravidelně již mnoho let a můžeme tak dosahovat i pokročilejších výsledků. Spousta spokojených klientů se 
+                    ke mně vrací i opakovaně.
+                    <strong> Vzhledem k nesporným benefitům silového tréninku si nesmírně vážím každého, kdo se rozhodne, ať už s mojí pomocí či jinak, silový trénink zařadit 
+                    do svého života.</strong>
+                    </p>
                 </span>
                 
 
