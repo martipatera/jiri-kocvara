9e2357cbf6ec393356a33bb16a99739d5d1c9953
Martin <martinaretap@gmail.com>
2024-11-04 16:13:21 +0100

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -14,13 +14,13 @@ function Footer() {
   return (
     <footer className='flex flex-col md:flex-row justify-between items-center gap-5 h-max px-3 font-bold bg-gray-500 '>
 
-        <div className='flex pt-5 md:py-5 order-1'>
+        <div className='flex pb-5 md:pb-0 order-3 mr-0 md:mr-2 lg:mr-5'>
         <Image src={Logo}
               height={65}
               width={75}
               alt="Logo stránky Jiřího Kočvary" 
               priority
-              className="sm:ml-10 ml-0 md:ml-0 lg:ml-0 hidden lg:block"
+              className="hidden lg:block"
           />
 
           <Image src={Logo}
@@ -34,7 +34,7 @@ function Footer() {
              
         </div>
         
-        <div className='pr-5 order-3 text-sm text-center md:text-right pb-5 md:pb-0'>
+        <div className='ml-0 md:ml-2 lg:ml-5 order-1 text-sm text-center md:text-left pt-5 md:py-5 '>
               <p>Jiří Kočvara - Coach</p>
               <p>IČO: 05430402</p>
               <p>U Trati 57</p>
@@ -42,7 +42,7 @@ function Footer() {
           </div> 
       
 
-      <div className='flex flex-row items-center gap-5 order-2 '>
+      <div className='flex flex-row items-center justify-center mx-auto order-2 '>
         <a href="https://martinpatera.cz/" target='_blank' className='text-sm'>Copyright ©2024 Martin Patera</a>
         
       </div>
