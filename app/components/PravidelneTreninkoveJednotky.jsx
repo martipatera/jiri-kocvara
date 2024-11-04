49aa31de957d8746eabf24f5d78d0dc045baf330
Martin <martinaretap@gmail.com>
2024-11-04 12:53:19 +0100

    no wrap

--- app/components/PravidelneTreninkoveJednotky.jsx
+++ app/components/PravidelneTreninkoveJednotky.jsx
@@ -63,7 +63,7 @@ function PravidelneTreninkoveJednotky() {
                     </span>
 
                     <span className="flex justify-center items-center mt-5 xl:mt-0 w-full md:w-[200px] focus:outline-none text-white bg-deep-orange-500  hover:bg-deep-orange-700 focus:ring-4 focus:ring-deep-orange-300 font-medium rounded-lg text-md px-5 py-2 me-2 mb-2 ">
-                        <button type="button" >Mám zájem </button>
+                        <button type="button" className="whitespace-nowrap" >Mám zájem </button>
                         <Popover></Popover>
                     </span>
                     
