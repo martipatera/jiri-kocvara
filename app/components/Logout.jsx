cf7ae3800161ccef9e38c10eeda7b9222498a6bf
Martin <martinaretap@gmail.com>
2024-11-04 15:10:23 +0100

    xl logout

--- app/components/Logout.jsx
+++ app/components/Logout.jsx
@@ -15,7 +15,7 @@ function Logout() {
   }
 
   return (
-    <div className='flex flex-col md:flex-row lg:flex-col gap-1 mr-2 md:mr-2'>
+    <div className='flex flex-col md:flex-row xl:flex-col gap-1 mr-2 md:mr-2'>
       <p className='text-center text-xs border-2 md:text-sm rounded-full p-1 xl:p-1 text-white'>{email} <small>{role}</small></p>
       <button className='text-center text-white hover:brightness-110 hover:scale-105 transition-all border-2 p-1 xl:p-1 rounded-full  whitespace-nowrap' onClick={handleLogout}>Odhlásit se</button>
     </div>
