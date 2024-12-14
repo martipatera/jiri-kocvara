8d94753f856c0d219a46fcada8644eff68b8f9f3
Martin <martinaretap@gmail.com>
2024-12-14 15:48:55 +0100

    px

--- app/(pages)/profil/page.jsx
+++ app/(pages)/profil/page.jsx
@@ -42,8 +42,8 @@ function Profil() {
               
  
 
-              <div className=' flex md:px-0 lg:hidden 2xl:flex 2xl:h-full flex-row px-2 justify-start items-center text-center lg:w-[500px] xl:w-[800px] lg:text-start text-lg md:text-lg flex-wrap break-words pt-10 xl:pt-0 pb-10'>
-                <p className='lg:px-5'> <strong>Kdo jsem a jak pracuji?</strong><br></br>
+              <div className=' flex md:px-5 lg:hidden 2xl:flex 2xl:h-full flex-row justify-start items-center text-center lg:w-[500px] xl:w-[800px] lg:text-start text-lg md:text-lg flex-wrap break-words pt-10 xl:pt-0 pb-10 '>
+                <p> <strong>Kdo jsem a jak pracuji?</strong><br></br>
                     Trénování v posilovně, vedení lidí k dosažení progresu a mnohdy i zlepšení kvality běžného života se věnuji stabilně již  <strong>více než 10 let.</strong> Věřím, 
                     že právě díky mojí filozofii, se kterou ke své práci přistupuji, mohu s lidmi pracovat full-time, aniž bych byl nucen mít k trénovaní ještě další práci. 
                     To mi za posledních několik let umožňuje neustále se dál vzdělávat a následně <strong>- to absolutně nejdůležitější –</strong>  aplikovat tyto znalosti v praxi s mými klienty, 
@@ -65,7 +65,7 @@ function Profil() {
                     </p>
               </div>
 
-              <div className='hidden pl-3 pb-5 lg:flex 2xl:hidden flex-row px-8 justify-start items-center text-center xl:w-full xl:px-10 lg:text-start text-lg md:text-lg flex-wrap break-words'>
+              <div className='hidden pl-3 pb-5 lg:flex 2xl:hidden flex-row px-8 justify-start items-center text-center xl:w-full xl:px-10 lg:text-start text-lg md:text-lg flex-wrap break-words '>
                   <p><strong>Kdo jsem a jak pracuji?</strong><br></br>
                     Trénování v posilovně, vedení lidí k dosažení progresu a mnohdy i zlepšení kvality běžného života se věnuji stabilně již <strong>více než 10 let.</strong> Věřím, 
                     že právě díky mojí filozofii, se kterou ke své práci přistupuji, mohu s lidmi pracovat full-time, aniž bych byl nucen mít k trénovaní ještě další práci. 
@@ -111,13 +111,11 @@ function Profil() {
 
               </div>
           </div>
-          <div className='rounded-lg flex flex-col justify-around items-center p-1 md:p-3  mx-2 lg:mx-5 2xl:px-5 2xl:mx-28 lg:flex-row-reverse mt-0 lg:mt-14'>
 
-              
-
+          <div className='rounded-lg flex flex-col justify-around items-center p-1 md:p-3  mx-2 lg:mx-10 2xl:px-10 2xl:mx-28 lg:flex-row-reverse mt-0 lg:mt-14'>
             
               <Image src={JirkaPoza}
-                  className='hidden lg:block rounded-xl w-full max-w-sm md:max-w-md lg:w-[500px] xl:w-max-w-2xl 2xl:max-w-2xl lg:order-1 mb-10'
+                  className='hidden lg:block rounded-xl w-full max-w-sm md:max-w-md lg:w-[500px] xl:w-max-w-2xl 2xl:max-w-2xl lg:order-1 mb-10 '
                   alt='Jiří Kočvara pózující s medailí za vítězství v soutěži'
                   quality={50}
                   >
