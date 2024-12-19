af0198a2f23400d4c0f75118b55c512b94a1b3e5
Martin <martinaretap@gmail.com>
2024-12-19 09:36:10 +0100

    sitemap

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-    siteUrl: 'https://www.jirikocvara.cz', // URL vašeho webu
-    generateRobotsTxt: true, // (volitelně) generování robots.txt
-    sitemapSize: 5000, // Maximální počet adres URL na jednu sitemapu
-  };
-  
-  module.exports = config;
-  
